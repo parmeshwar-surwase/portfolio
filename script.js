c8b14bf8730761f981ae71761f75d87eca2e2df3
Parmeshwar Surwase <parmeshwar.surwase.dev@gmail.com>
2026-02-23 15:05:45 +0000

    added time

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 alert("Welcome!");
+function updateTime() {
+    const now = new Date();
+    document.getElementById("clock").innerHTML =
+        now.toLocaleTimeString();
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+;
